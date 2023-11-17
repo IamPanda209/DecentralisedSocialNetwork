a9707d44e2fe944ae9809ad0d4d15aa0a2c8e44a
IamPanda209 <116966701+IamPanda209@users.noreply.github.com>
2023-11-17 18:42:53 +0530

    Add files via upload

---
+++ LocallyHostedSN.py
@@ -0,0 +1,197 @@
+import tkinter as tk
+import json
+import hashlib
+
+class User:
+    def __init__(self, username):
+        self.username = username
+        self.posts = []
+        self.friends = []
+
+    def create_post(self, content):
+        self.posts.append(content)
+        print(f"Post created by {self.username}.")
+
+    def add_friend(self, friend):
+        self.friends.append(friend)
+        print(f"{self.username} and {friend} are now friends.")
+
+class Network:
+    def __init__(self):
+        self.users = {}
+        self.load_user_data()
+
+    def create_user(self, username, password):
+        if username not in self.users:
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            self.users[username] = {'password': hashed_password, 'user': User(username)}
+            print(f"User {username} created.")
+            self.save_user_data()
+            return True
+        else:
+            print("Username already exists.")
+            return False
+
+    def login_user(self, username, password):
+        if username in self.users and 'password' in self.users[username]:
+            stored_password = self.users[username]['password']
+            hashed_password = hashlib.sha256(password.encode()).hexdigest()
+            if stored_password == hashed_password:
+                print(f"User {username} logged in.")
+                return True
+        print("Invalid username or password.")
+        return False
+
+    def make_post(self, username, content):
+        if username in self.users:
+            user = self.users[username]['user']
+            user.create_post(content)
+            self.save_user_data()
+        else:
+            print("User not found.")
+
+    def add_friendship(self, username, friend):
+        if username in self.users and friend in self.users:
+            user = self.users[username]['user']
+            friend_user = self.users[friend]['user']
+            user.add_friend(friend)
+            friend_user.add_friend(username)
+            self.save_user_data()
+        else:
+            print("User(s) not found.")
+
+    def delete_user(self, username):
+        if username in self.users:
+            del self.users[username]
+            self.save_user_data()
+            print(f"User {username} has been deleted.")
+        else:
+            print("User not found.")
+
+    def save_user_data(self):
+        with open("user_data.json", "w") as file:
+            user_data = {
+                username: {
+                    'password': data['password'],
+                    'posts': data['user'].posts,
+                    'friends': data['user'].friends
+                }
+                for username, data in self.users.items()
+            }
+            json.dump(user_data, file)
+
+    def load_user_data(self):
+        try:
+            with open("user_data.json", "r") as file:
+                user_data = json.load(file)
+                for username, data in user_data.items():
+                    new_user = User(username)
+                    new_user.posts = data["posts"]
+                    new_user.friends = data["friends"]
+                    self.users[username] = {'password': data['password'], 'user': new_user}
+        except FileNotFoundError:
+            pass
+
+class LoginWindow:
+    def __init__(self, master, network):
+        self.master = master
+        self.master.title("Login")
+        self.master.configure(bg="black") 
+
+        self.network = network
+
+        self.label_username = tk.Label(master, text="Enter Username:", font=("Arial", 16), bg="black", fg="white")
+        self.entry_username = tk.Entry(master, font=("Arial", 16))
+        self.label_password = tk.Label(master, text="Enter Password:", font=("Arial", 16), bg="black", fg="white")
+        self.entry_password = tk.Entry(master, font=("Arial", 16), show="*")
+        self.button_login = tk.Button(master, text="Login", command=self.login, bg="light green", fg="black", font=("Arial", 16))
+
+        self.label_username.grid(row=0, column=0, padx=10, pady=10)
+        self.entry_username.grid(row=0, column=1, padx=10, pady=10)
+        self.label_password.grid(row=1, column=0, padx=10, pady=10)
+        self.entry_password.grid(row=1, column=1, padx=10, pady=10)
+        self.button_login.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
+
+    def login(self):
+        username = self.entry_username.get()
+        password = self.entry_password.get()
+        if self.network.login_user(username, password):
+            self.master.withdraw()
+            account_window = AccountWindow(tk.Toplevel(self.master), self.network, self.master)
+            account_window.set_current_user(username)
+
+class AccountWindow:
+    def __init__(self, master, network, login_window):
+        self.master = master
+        self.master.title("Account")
+        self.master.configure(bg="black") 
+
+        self.network = network
+        self.login_window = login_window
+        self.current_user = None
+
+        self.label_username = tk.Label(master, text="", font=("Arial", 16), bg="black", fg="white")
+        self.entry_content = tk.Entry(master, font=("Arial", 16))
+        self.entry_friend = tk.Entry(master, font=("Arial", 16))
+        self.button_make_post = tk.Button(master, text="Make Post", command=self.make_post, bg="light green", fg="black", font=("Arial", 16))
+        self.button_add_friendship = tk.Button(master, text="Add Friendship", command=self.add_friendship, bg="orange", fg="black", font=("Arial", 16))
+        self.button_display_friends_info = tk.Button(master, text="Display Friends Info", command=self.display_friends_info, bg="light blue", fg="black", font=("Arial", 16))
+        self.button_display_posts_info = tk.Button(master, text="Display Posts Info", command=self.display_posts_info, bg="light yellow", fg="black", font=("Arial", 16))
+        self.button_logout = tk.Button(master, text="Logout", command=self.logout, bg="red", fg="black", font=("Arial", 16))
+
+        self.label_username.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
+        self.entry_content.grid(row=1, column=0, padx=10, pady=10)
+        self.entry_friend.grid(row=1, column=1, padx=10, pady=10)
+        self.button_make_post.grid(row=2, column=0, padx=10, pady=10)
+        self.button_add_friendship.grid(row=2, column=1, padx=10, pady=10)
+        self.button_display_friends_info.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
+        self.button_display_posts_info.grid(row=4, column=0, columnspan=2, padx=10, pady=10)
+        self.button_logout.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
+
+    def set_current_user(self, username):
+        self.current_user = username
+        self.label_username.config(text=f"Logged in as: {username}")
+
+    def make_post(self):
+        content = self.entry_content.get()
+        if self.current_user:
+            self.network.make_post(self.current_user, content)
+
+    def add_friendship(self):
+        friend = self.entry_friend.get()
+        if self.current_user:
+            self.network.add_friendship(self.current_user, friend)
+
+    def display_friends_info(self):
+        if self.current_user:
+            user = self.network.users[self.current_user]['user']
+            friend_info_window = tk.Toplevel(self.master)
+            friend_info_window.title("Friends Information")
+            friend_info_window.configure(bg="black")
+
+            info_label = tk.Label(friend_info_window, text=f"Friends of {self.current_user}: {', '.join(user.friends)}", fg="white", bg="black", font=("Arial", 16))
+            info_label.pack(padx=20, pady=10)
+
+    def display_posts_info(self):
+        if self.current_user:
+            user = self.network.users[self.current_user]['user']
+            posts_info_window = tk.Toplevel(self.master)
+            posts_info_window.title("Posts Information")
+            posts_info_window.configure(bg="black")
+
+            info_label = tk.Label(posts_info_window, text=f"Posts by {self.current_user}: {', '.join(user.posts)}", fg="white", bg="black", font=("Arial", 16))
+            info_label.pack(padx=20, pady=10)
+
+    def logout(self):
+        self.master.destroy()
+        self.login_window.deiconify()
+
+    def show_window(self):
+        self.master.deiconify()
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.configure(bg="black")
+    network = Network()
+    login_window = LoginWindow(root, network)
+    root.mainloop()
